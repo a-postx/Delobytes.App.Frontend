0296f61d1f530d304fc840d1c3afab8b17f5e041
Алексей Якубин <2412719@mail.ru>
2026-08-31 08:19:49 +0400

    Поправлен компонент копирования

--- src/components/ui/copy-button/CopyButton.vue
+++ src/components/ui/copy-button/CopyButton.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { Copy, Check } from 'lucide-vue-next'
+import { ref } from 'vue'
 import type { HTMLAttributes } from 'vue'
 import { Button } from '@/components/ui/button'
 import { Tooltip, TooltipContent, TooltipTrigger, TooltipProvider } from '@/components/ui/tooltip'
