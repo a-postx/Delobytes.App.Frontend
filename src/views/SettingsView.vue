9d8eacb6904c714c553ca67fddcd13b38af832d5
Алексей Якубин <2412719@mail.ru>
2026-08-31 20:43:59 +0400

    Revert "Поправлена тёмная тема"
    This reverts commit 6f395bc90861491854c610ccee356c660f9335dc.

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -43,18 +43,14 @@ const userEmail = computed(() => currentUser.value?.email ?? '')
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <RadioGroup
-          class="flex flex-col gap-2.5"
-          :model-value="theme"
-          @update:model-value="(v) => setTheme(v as Theme)"
-        >
-          <div class="flex items-center">
+        <RadioGroup :model-value="theme" @update:model-value="(v) => setTheme(v as Theme)">
+          <div class="flex items-center space-x-2">
             <RadioGroupItem id="light" value="light" />
-            <Label for="light" class="cursor-pointer font-normal pl-[15px]">Светло</Label>
+            <Label for="light" class="cursor-pointer font-normal">Светло</Label>
           </div>
-          <div class="flex items-center">
+          <div class="flex items-center space-x-2">
             <RadioGroupItem id="dark" value="dark" />
-            <Label for="dark" class="cursor-pointer font-normal pl-[15px]">Темно</Label>
+            <Label for="dark" class="cursor-pointer font-normal">Темно</Label>
           </div>
         </RadioGroup>
       </CardContent>
