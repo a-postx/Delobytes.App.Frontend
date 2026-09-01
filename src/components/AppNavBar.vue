d3fb98776d28e7318e21ac03749c5a46f936259e
Алексей Якубин <2412719@mail.ru>
2026-09-01 15:07:09 +0400

    Поправлена тема

--- src/components/AppNavBar.vue
+++ src/components/AppNavBar.vue
@@ -19,7 +19,7 @@ const breadcrumbLabel = computed<string>(() => {
 </script>
 
 <template>
-  <header class="flex h-14 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+  <header class="flex h-14 shrink-0 items-center gap-2 border-b border-border transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
     <div class="flex items-center gap-2 px-4">
       <SidebarTrigger class="-ml-1" />
       <Separator
