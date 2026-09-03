b135c2029416affcbf840ba8e08dc6b38e0fee01
Алексей Якубин <2412719@mail.ru>
2026-09-03 05:52:44 +0400

    поправлен выбор тенанта

--- src/components/sidebar/TenantSwitcher.vue
+++ src/components/sidebar/TenantSwitcher.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { computed } from 'vue'
-import type { Component } from 'vue'
 import { ChevronsUpDown, Building2 } from 'lucide-vue-next'
 import {
   DropdownMenu,
