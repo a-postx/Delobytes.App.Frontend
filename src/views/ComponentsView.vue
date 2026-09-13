c3e9713384029590c26d374cac0581ca988461e2
Алексей Якубин <2412719@mail.ru>
2026-09-13 13:41:46 +0400

    Revert "Поправлены компоненты снова"
    This reverts commit 95c4bc4758d60680e3d2cb2f0c194c88ea72d619.

--- src/views/ComponentsView.vue
+++ src/views/ComponentsView.vue
@@ -376,7 +376,7 @@ const selectClass = 'flex h-10 w-full items-center justify-between rounded-md bo
         <DialogContent :class="dialogContentClass">
           <DialogTitle>Новый компонент</DialogTitle>
           <DialogDescription>
-            Добавление нового компонента
+            Добавление нового компонента в справочник
           </DialogDescription>
 
           <div class="flex flex-col gap-4 py-4">
@@ -386,7 +386,7 @@ const selectClass = 'flex h-10 w-full items-center justify-between rounded-md bo
                 id="name"
                 v-model="form.name"
                 :class="inputClass"
-                placeholder=""
+                placeholder="Введите название"
               />
             </div>
 
@@ -396,7 +396,7 @@ const selectClass = 'flex h-10 w-full items-center justify-between rounded-md bo
                 id="description"
                 v-model="form.description"
                 :class="inputClass"
-                placeholder=""
+                placeholder="Введите описание (необязательно)"
               />
             </div>
 
@@ -414,7 +414,7 @@ const selectClass = 'flex h-10 w-full items-center justify-between rounded-md bo
             </div>
 
             <div :class="fieldClass">
-              <Label for="pricePerUnit">Цена за единицу *</Label>
+              <Label for="pricePerUnit">Цена за единицу</Label>
               <Input
                 id="pricePerUnit"
                 v-model.number="form.pricePerUnit"
@@ -426,7 +426,7 @@ const selectClass = 'flex h-10 w-full items-center justify-between rounded-md bo
             </div>
 
             <div :class="fieldClass">
-              <Label for="supplier">Поставщик</Label>
+              <Label for="supplier">Поставщик (необязательно)</Label>
               <select
                 id="supplier"
                 v-model="form.supplierId"
@@ -442,7 +442,7 @@ const selectClass = 'flex h-10 w-full items-center justify-between rounded-md bo
             </div>
 
             <div :class="fieldClass">
-              <Label for="validFrom">Цена действует с *</Label>
+              <Label for="validFrom">Дата начала действия цены</Label>
               <Input
                 id="validFrom"
                 v-model="form.validFrom"
@@ -479,7 +479,7 @@ const selectClass = 'flex h-10 w-full items-center justify-between rounded-md bo
 
           <div class="flex flex-col gap-4 py-4">
             <div :class="fieldClass">
-              <Label for="newPricePerUnit">Цена за единицу *</Label>
+              <Label for="newPricePerUnit">Цена за единицу</Label>
               <Input
                 id="newPricePerUnit"
                 v-model.number="priceForm.pricePerUnit"
@@ -491,7 +491,7 @@ const selectClass = 'flex h-10 w-full items-center justify-between rounded-md bo
             </div>
 
             <div :class="fieldClass">
-              <Label for="newSupplier">Поставщик</Label>
+              <Label for="newSupplier">Поставщик (необязательно)</Label>
               <select
                 id="newSupplier"
                 v-model="priceForm.supplierId"
@@ -507,7 +507,7 @@ const selectClass = 'flex h-10 w-full items-center justify-between rounded-md bo
             </div>
 
             <div :class="fieldClass">
-              <Label for="newValidFrom">Действует с *</Label>
+              <Label for="newValidFrom">Дата начала действия</Label>
               <Input
                 id="newValidFrom"
                 v-model="priceForm.validFrom"
